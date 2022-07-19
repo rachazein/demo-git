ed6d62bd325e9b538e8ca1efe0917d223b73dd8b
RAcha ElZein <el.zein@hotmail.fr>
2022-07-19 15:08:29 +0200

    correction synthax python step1

--- main.py
+++ main.py
@@ -2,15 +2,19 @@
 #C'est une suite définie par Un = Un-1 + Un-2
 
 
-def fibonacci(n:int):
-   if n==1:
-       return 1
-   if n==2:
-       return 1
-   u0=0
-   u1=1
-   for x in range (3,n+1):
-        un=u0+u1
-        u0=u1
-        u1=un
-        return un
+def fibonacci(nb_entier:int):
+    """
+    une fonction qui calcule la suite de fibonacci pour un nombre entier 
+    elle prend un seul argument n de type int
+    """
+    if nb_entier==1:
+        return 1
+    if nb_entier==2:
+        return 1
+    elt_0=0
+    elt_1=1
+    for x in range (3,nb_entier+1):
+         elt_n=elt_0+elt_1
+         elt_0=elt_1
+         elt_1=elt_n
+         return elt_n
