0ad920b797d3137cee29e3ced7fc4d63c74c542b
RAcha ElZein <el.zein@hotmail.fr>
2022-07-19 15:01:23 +0200

    fibonacci function

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
-"""
-c'est une documentation
-"""
-print("Hello world!!!")
+#Ecrire l'algorithme de la suite de fibonacci:
+#C'est une suite définie par Un = Un-1 + Un-2
+
+
+def fibonacci(n:int):
+   if n==1:
+       return 1
+   if n==2:
+       return 1
+   u0=0
+   u1=1
+   for x in range (3,n+1):
+        un=u0+u1
+        u0=u1
+        u1=un
+        return un
